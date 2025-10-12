ead3799edd5bf2bc2d978ab095b98c6203155dc1
THB-L85 <luis.dr85a@gmail.com>
2025-10-11 23:13:46 -0600

    add: dark mode in progress (blog)

---
+++ src/js/app.js
@@ -0,0 +1,53 @@
+document.addEventListener('DOMContentLoaded', function (){
+    darkMode();
+})
+
+function darkMode(){
+    const prefiereDarkMode = window.matchMedia('(prefers-color-scheme: dark)');
+    const toggleButton = document.querySelector('.dark-mode-boton').firstElementChild;
+    console.log(toggleButton);
+        
+    // console.log(prefiereDarkMode.matches);
+
+    if(prefiereDarkMode.matches) {
+        document.body.classList.add('dark-mode');
+        toggleButton.src = 'src/img/brightness.png'
+    } else {
+        document.body.classList.remove('dark-mode');
+        toggleButton.src = 'src/img/moon.png'
+
+    }
+
+    prefiereDarkMode.addEventListener('change', function() {
+        if(prefiereDarkMode.matches) {
+            document.body.classList.add('dark-mode');
+        toggleButton.src = 'src/img/brightness.png'
+        } else {
+            document.body.classList.remove('dark-mode');
+        toggleButton.src = 'src/img/moon.png'
+
+        }
+    });
+
+    const botonDarkMode = document.querySelector('.dark-mode-boton');
+    botonDarkMode.addEventListener('click', function() {
+        document.body.classList.toggle('dark-mode');
+        console.log('toggle');
+        
+        // Cambiar imagen dependiendo de si está en modo oscuro
+        if (document.body.classList.contains('dark-mode')) {
+        console.log('toggle has dark');
+
+            toggleButton.src = 'src/img/brightness.png'; // Imagen para modo oscuro
+        console.log('dark', toggleButton);
+
+        } else {
+        console.log('toggle light');
+
+            toggleButton.src = 'src/img/moon.png'; // Imagen para modo claro
+        console.log('light', toggleButton);
+
+        }
+
+    });
+}
