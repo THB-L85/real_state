55072d0ea0f26bb27fdc638862f922a1d555f661
THB-L85 <luis.dr85a@gmail.com>
2026-01-06 00:15:29 -0600

    add: gulpfile prepare for prod

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,17 @@
-import {src, dest, watch, series} from 'gulp'
-import * as dartSass from 'sass'
-import gulpSass from 'gulp-sass'
+import { src, dest, watch, parallel } from 'gulp';
+import dartSass from 'sass';
+import gulpSass from 'gulp-sass';
+import autoprefixer from 'autoprefixer';
+import postcss from 'gulp-postcss';
+import sourcemaps from 'gulp-sourcemaps';
+import cssnano from 'cssnano';
+import concat from 'gulp-concat';
+import terser from 'gulp-terser';
+import rename from 'gulp-rename';
+import imagemin from 'gulp-imagemin';
+import notify from 'gulp-notify';
+import cache from 'gulp-cache';
+import webp from 'gulp-webp';
 
 const sass = gulpSass(dartSass);
 
@@ -8,47 +19,59 @@ const paths = {
     scss: 'src/scss/**/*.scss',
     js: 'src/js/**/*.js',
     imagenes: 'src/img/**/*'
-}
+};
 
+// CSS
 export function css() {
-    return src(paths.scss)
-        // .pipe(sourcemaps.init())
-        .pipe(sass())
-        // .pipe(postcss([autoprefixer(), cssnano()]))
-        // // .pipe(postcss([autoprefixer()]))
-        // .pipe(sourcemaps.write('.'))
-        .pipe(dest('build/css'));
+    return src(paths.scss, { sourcemaps: true })
+        .pipe(sass().on('error', sass.logError))
+        .pipe(postcss([
+            autoprefixer(),
+            cssnano()
+        ]))
+        .pipe(dest('build/css', { sourcemaps: '.' }));
 }
 
+// JavaScript
 export function javascript() {
-    return src(paths.js)
-    //   .pipe(sourcemaps.init())
-    //   .pipe(concat('bundle.js'))
-    //   .pipe(terser())
-    //   .pipe(sourcemaps.write('.'))
-    //   .pipe(rename({ suffix: '.min' }))
-      .pipe(dest('./build/js'))
+    return src(paths.js, { sourcemaps: true })
+        .pipe(concat('bundle.js'))
+        .pipe(terser())
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(dest('build/js', { sourcemaps: '.' }));
 }
 
-function imagenes() {
+// Imágenes
+export function imagenes() {
     return src(paths.imagenes)
-        // .pipe(cache(imagemin({ optimizationLevel: 3 })))
+        .pipe(cache(
+            imagemin({ optimizationLevel: 3 })
+        ))
         .pipe(dest('build/img'))
-        // .pipe(notify('Imagen Completada' ));
+        .pipe(notify({ message: 'Imágenes optimizadas' }));
 }
 
-function versionWebp() {
+// WebP
+export function versionWebp() {
     return src(paths.imagenes)
-        // .pipe(webp())
+        .pipe(webp())
         .pipe(dest('build/img'))
-        // .pipe(notify({ message: 'Imagen Completada' }));
+        .pipe(notify({ message: 'Imágenes WebP creadas' }));
 }
 
+// Watch
 export function watchArchivos() {
     watch(paths.scss, css);
     watch(paths.js, javascript);
-    // watch(paths.imagenes, imagenes);
-    // watch(paths.imagenes, versionWebp);
+    watch(paths.imagenes, imagenes);
+    watch(paths.imagenes, versionWebp);
 }
 
-export default series(css, javascript, watchArchivos); 
+// Default
+export default parallel(
+    css,
+    javascript,
+    imagenes,
+    versionWebp,
+    // watchArchivos
+);
